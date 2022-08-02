234bb6447e0af9d1a1b471d4361f2224f58b554a
Stanislav Tutunaru <tutunaru.stanislav@yandex.ru>
2022-08-02 14:45:45 +0500

    - change redoc

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        'redoc/', TemplateView.as_view(template_name='templates/redoc.html'),
+        name='redoc'
+    ),
     path('api/', include('api.urls')),
 ]
